bc16c8b2a3bf8ee31291670449227792ed62623d
funnyxpro90u-dev <funnyxpro90u@gmail.com>
2026-05-05 03:45:39 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,6 +5,6 @@ API_ID = int(os.environ.get("API_ID", "30296254"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8620034214:AAH1BHz9xdV6XNX0DZZUWkJqv97qp9rh-Kg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7651099703:AAGQU1XFQoizUAwZFzFzTnU4wqguOHROo1w")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7660916897"))
