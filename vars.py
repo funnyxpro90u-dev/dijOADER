215160c4b999f2ede513317e35430e550c6e9844
funnyxpro90u-dev <funnyxpro90u@gmail.com>
2026-05-05 03:23:45 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,10 @@
 import os
-
-API_ID    = os.environ.get("API_ID", "30296254")
-API_HASH  = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8614201321:AAFYogtipBDjlWBhTQGXJTcYuX3co4S0t4c") 
+from os import getenv
+# ---------------R---------------------------------
+API_ID = int(os.environ.get("API_ID", "30296254"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+# ----------------D--------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8620034214:AAH1BHz9xdV6XNX0DZZUWkJqv97qp9rh-Kg")
+# ------------------X------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "7660916897"))
